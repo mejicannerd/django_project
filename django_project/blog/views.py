2a8429e8485d1d3e7632e80b76ae85f9d0760c83
mejicannerd <102626387+mejicannerd@users.noreply.github.com>
2023-12-12 15:08:48 -0800

    Added: Login, Logout, register

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import  login_required
 from .models import Post
 from .forms import PostForm
 
+@login_required
 def delete_post(request, id):
-    post = get_object_or_404(Post, pk=id)
+    queryset = Post.objects.filter(author=request.user)
+    post = get_object_or_404(queryset, pk=id)
     context = {'post': post}    
     
     if request.method == 'GET':
@@ -12,10 +15,12 @@ def delete_post(request, id):
     elif request.method == 'POST':
         post.delete()
         messages.success(request,  'The post has been deleted successfully.')
-        return redirect('posts')
+        return redirect('posts')        
 
+@login_required    
 def edit_post(request, id):
-    post = get_object_or_404(Post, id=id)
+    queryset = Post.objects.filter(author=request.user)
+    post = get_object_or_404(queryset, pk=id)
 
     if request.method == 'GET':
         context = {'form': PostForm(instance=post), 'id': id}
@@ -29,8 +34,9 @@ def edit_post(request, id):
             return redirect('posts')
         else:
             messages.error(request, 'Please correct the following errors:')
-            return render(request,'blog/post_form.html',{'form':form})  
+            return render(request,'blog/post_form.html',{'form':form})
 
+@login_required
 def create_post(request):
     if request.method == 'GET':
         context = {'form': PostForm()}
@@ -38,18 +44,20 @@ def create_post(request):
     elif request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.author = request.user
+            user.save()
             messages.success(request, 'The post has been created successfully.')
             return redirect('posts')
         else:
             messages.error(request, 'Please correct the following errors:')
-            return render(request,'blog/post_form.html',{'form':form})
-
+            return render(request,'blog/post_form.html',{'form':form})  
+    
 
 def home(request):
     posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(request, 'blog/home.html', context)
+    context = {'posts': posts  }
+    return render(request,'blog/home.html', context)   
 
 
 def about(request):
